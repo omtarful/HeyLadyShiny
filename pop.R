4caa963e24e5b10995a18706ff06b9a376cbd63b
omtarful <omtarful@gmail.com>
2024-06-21 22:35:41 +0300

    Points 1-6 Fixed

--- pop.R
+++ pop.R
@@ -7,6 +7,8 @@ library(tidyverse)
 library(dplyr)
 library(lubridate)
 library(ggtext)
+library(memoise)
+#Kareen's code
 fluency_stages <- data.frame(
   Stage = 3:9,
   Label = c("I can understand but I'm afraid to speak",
@@ -28,7 +30,8 @@ calculate_progress <- function(stage, weekly_hours) {
   current_hours <- fluency_stages$CumulativeHours[fluency_stages$Stage == stage]
   day_increment <- day(1)
   
- 
+  # Initialize an empty list to store the rows
+  rows <- list()
   
   # Loop through each week and assign the correct stage and date
   current_stage_index <- which(fluency_stages$Stage == stage)
@@ -40,10 +43,6 @@ calculate_progress <- function(stage, weekly_hours) {
   end_date <- start_date + weeks(total_weeks)
   daily_hours = weekly_hours/7
   current_hours = fluency_stages$CumulativeHours[fluency_stages$Stage == stage]
-  # Initialize an empty list to store the rows
-  estimated_iterations <- as.integer((end_date - current_date) / day_increment) + 1
-  
-  rows <- vector("list", estimated_iterations)
   while(current_stage_index <= nrow(fluency_stages) && current_date <= end_date) {
     current_hours <- current_hours + daily_hours
     if (current_stage_index < nrow(fluency_stages) && current_hours > fluency_stages$CumulativeHours[current_stage_index + 1]) {
@@ -68,28 +67,25 @@ calculate_progress <- function(stage, weekly_hours) {
   
   # Calculate progress
   progress <- numeric(nrow(weekly_data))
-  # Extract the relevant columns and precompute indices
-  stage_indices <- weekly_data$Stage - 2
-  next_stage_indices <- weekly_data$Stage - 1
+  for (i in seq_len(nrow(weekly_data))) {
+    stage <- weekly_data$Stage[i]
+    cum_hours <- fluency_stages$CumulativeHours[stage - 2]
+    hours_spent <- weekly_data$HoursSpent[i]
+    current_stage_cum_hours <- fluency_stages$CumulativeHours[stage - 2]
+    next_stage = fluency_stages$CumulativeHours[stage - 1]
+    cat(stage, cum_hours, hours_spent, current_stage_cum_hours, next_stage, "\n")
+    p <- ((hours_spent - current_stage_cum_hours) / ( next_stage - current_stage_cum_hours)) + stage
+    progress[i] <- p
   
-  # Vectorize access to the 'CumulativeHours' for current and next stages
-  current_stage_cum_hours <- fluency_stages$CumulativeHours[stage_indices]
-  next_stage_cum_hours <- fluency_stages$CumulativeHours[next_stage_indices]
-  
-  # Compute 'p' for all rows without the loop
-  p <- ((weekly_data$HoursSpent - current_stage_cum_hours) / 
-          (next_stage_cum_hours - current_stage_cum_hours)) + weekly_data$Stage
-  
-  # If 'progress' needs to be updated or stored
-  weekly_data$progress <- p
+  }
   weekly_data$Progress <- progress
   weekly_data$Date = as.Date(weekly_data$Date)
   return(list(weekly_data, fluency_stages))
 }
 
-library("memoise")
-
 calculate_progress = memoise(calculate_progress)
+
+
 label_colors = c(
   "Starting to speak" = "#D8E8E9",
   "Speak without fear" = "#B0D0D3",
@@ -105,7 +101,6 @@ ui <- fluidPage(
   # Custom CSS to increase the font size and width of selectInput elements
   tags$head(
     tags$style(HTML("
-     
       .select-input-lg .selectize-input {
         font-size: 24px;
 
@@ -124,6 +119,7 @@ ui <- fluidPage(
       body {
         font-family: Helvetica;
         margin-left: 50px;
+        line-height:1;
       }
     
       .select-input-lg .selectize-dropdown {
@@ -141,7 +137,7 @@ ui <- fluidPage(
         background: url('triangle.svg') no-repeat 98% center !important; /* Adjust background position */
         background-size: 13px 13px !important; /* Specify a size for the background image */
         background-color: white !important; /* Ensure background color is set */
-        height: 40px; /* Set a fixed height for the input box */
+       # height: 40px; /* Set a fixed height for the input box */
         line-height: 40px; /* Ensure text is vertically centered */
         padding: 0 12px; /* Adjust padding to vertically center the text */
 
@@ -181,7 +177,7 @@ ui <- fluidPage(
       
      .select-input-wide-small .selectize-input {
         width: 140px !important;
-        height: 40px; /* Ensure there's enough height */
+        #height: 40px; /* Ensure there's enough height */
         border-radius: 100px !important;
         border: 2px solid #D17329 !important;
         color: #D17329 !important;
@@ -229,6 +225,7 @@ ui <- fluidPage(
       
       h3 {
         margin-right: 5px;
+       
       }
       
             
@@ -285,7 +282,7 @@ ui <- fluidPage(
            ),
            # Third line with final message
            tags$div(
-             style = "display: flex; align-items: center; margin-bottom: 50px; margin-right: 20px;",
+             style = "display: flex; align-items: center; margin-bottom: 10px; margin-right: 20px;",
              h3(htmlOutput("monthsText")),
              h3(" to speak English fluently & effortlessly.")
            )
@@ -293,10 +290,10 @@ ui <- fluidPage(
     # Upper Right Section (4 columns)
     column(4, 
            # Using HTML to apply specific styles to parts of the text
-           h3(htmlOutput("savingsText")),
+           h3(htmlOutput("savingsText"),style=" margin-top: 24px; line-height:2;"),
            # CSS for increasing line spacing globally within this column
            tags$head(
-             tags$style(HTML(".shiny-text-output { line-height: 2; }"))  # Increase line height for better spacing
+             tags$style(HTML(".shiny-text-output { line-height: 2;}"))  # Increase line height for better spacing
            )
 
         
@@ -305,17 +302,19 @@ ui <- fluidPage(
   
   # Second Row
   fluidRow(
+
     # Bottom Left Section (8 columns)
     column(8, 
            tags$div(
-           card(plotOutput("fluencyGraph", width = "90%"), full_screen = TRUE)
+           style= "margin-top: 16px;",
+           plotOutput("fluencyGraph", width = "80%")
            )
     ),
     # Bottom Right Section (4 columns)
     column(4, 
            tags$div(
-           style = "margin-right: 50px;",
-           card(plotOutput("costComparison", width = "100%"), full_screen = TRUE)
+          style = "margin-right: 50px;",
+           plotOutput("costComparison", width = "100%")
            )
     )
   )
@@ -329,7 +328,6 @@ server <- function(input, output) {
   toListen <- reactive({
     list(input$ability,input$hours)
   })
-  
   observeEvent( list(input$ability,input$hours), {
     # Convert ability input to corresponding stage
     stage <- switch(input$ability,
@@ -367,46 +365,46 @@ server <- function(input, output) {
     y_labels <- fluency_stages$GraphLabel
     integer_points <- progress_data %>%
       group_by(GraphLabel) %>%
-      dplyr::filter(HoursSpent == min(HoursSpent)) %>%
+      filter(HoursSpent == min(HoursSpent)) %>%
       ungroup()  
     integer_points$HoursSpent = round(integer_points$HoursSpent)
     # Placeholder for fluency prediction graph
-    
-      fluency_plot = ggplot(progress_data, aes(x = Date, y = HoursSpent)) +
-        geom_line(color = "black", size = 0.5) +  # Or geom_smooth, depending on your preference
-        scale_y_continuous(
-          breaks = y_breaks[(stage-2):nrow(fluency_stages)],  # Your custom breaks
-          labels = y_labels[(stage-2):nrow(fluency_stages)],  # Corresponding labels,
-          expand = expansion(mult = c(0, 0.1)),
-          limits = c( min(y_breaks[(stage-2):nrow(fluency_stages)]), NA) 
-        ) +
-        geom_ribbon(aes( ymin = min(HoursSpent), ymax = HoursSpent, fill = factor(GraphLabel)), alpha = 0.6, show.legend = FALSE, outline.type =  "lower") +
-        scale_fill_manual(values = label_colors[(stage-2):7]) +
-        geom_point(data = integer_points[-1,], shape =21, fill = "white", color = "black", size = 5) +
-        scale_x_date(
-          breaks = c(min(progress_data$Date), max(progress_data$Date)),  # Set breaks to only the first and last date
-          labels = function(x) ifelse(x == min(progress_data$Date), "Today", format(x, "%b\n%Y"))
-        ) + 
-        labs(y = "FLUENCY", x="TIME") +
-        theme_minimal() +
-        theme(
-          panel.grid.major.x = element_blank(),
-          panel.grid.minor.x = element_blank(),
-          panel.grid.major.y = element_line(color = "gray"),
-          panel.grid.minor.y = element_blank(),
-          axis.title =element_text(size = 20),
-          axis.title.y = element_text(margin = margin(t = 0, r = -2, b = 0, l = 0), face = "bold"), # Adjust r (right) margin to bring the title closer to the axis
-          axis.title.x = element_text(face = "bold") ,
-          axis.text= element_text(size = 15),
-          axis.line.x = element_line(size = 1, linetype = "solid", colour = "black"),
-          axis.line.y = element_line(size = 1, linetype = "solid", colour = "black"),
-          plot.margin = margin(t = 20, r = 20, b = 20, l = 20) # Adjust plot margins if needed
-          
-        ) 
-      cached_fluency_graph(fluency_plot)
+    fluency_plot = ggplot(progress_data, aes(x = Date, y = HoursSpent)) +
+      geom_line(color = "black", size = 0.5) +  # Or geom_smooth, depending on your preference
+      scale_y_continuous(
+        breaks = y_breaks[(stage-2):nrow(fluency_stages)],  # Your custom breaks
+        labels = y_labels[(stage-2):nrow(fluency_stages)],  # Corresponding labels,
+        expand = expansion(mult = c(0, 0.1)),
+        limits = c( min(y_breaks[(stage-2):nrow(fluency_stages)]), NA) 
+      ) +
+      geom_ribbon(aes( ymin = min(HoursSpent), ymax = HoursSpent, fill = factor(GraphLabel)), alpha = 0.6, show.legend = FALSE, outline.type =  "lower") +
+      scale_fill_manual(values = label_colors[(stage-2):7]) +
+      geom_point(data = integer_points[-1,], shape =21, fill = "white", color = "black", size = 5) +
+      scale_x_date(
+        breaks = c(min(progress_data$Date), max(progress_data$Date)),  # Set breaks to only the first and last date
+        labels = function(x) ifelse(x == min(progress_data$Date), "Today", format(x, "%b\n%Y"))
+      ) + 
+      labs(y = "FLUENCY", x="TIME") +
+      theme_minimal() +
+      theme(
+        panel.grid.major.x = element_blank(),
+        panel.grid.minor.x = element_blank(),
+        panel.grid.major.y = element_line(color = "gray"),
+        panel.grid.minor.y = element_blank(),
+        axis.title =element_text(size = 20),
+        axis.title.y = element_text(margin = margin(t = 0, r = -2, b = 0, l = 0), face = "bold"), # Adjust r (right) margin to bring the title closer to the axis
+        axis.title.x = element_text(face = "bold") ,
+        axis.text= element_text(size = 15),
+        axis.line.x = element_line(size = 1, linetype = "solid", colour = "black"),
+        axis.line.y = element_line(size = 1, linetype = "solid", colour = "black"),
+        plot.margin = margin(t = 20, r = 20, b = 20, l = 20) # Adjust plot margins if needed
+        
+      ) 
+    cached_fluency_graph(fluency_plot)
     output$fluencyGraph <- renderPlot({
       cached_fluency_graph()
-
+    
+   
 
     })
     min_date <- min(progress_data$Date)
@@ -433,48 +431,50 @@ server <- function(input, output) {
     
     
     data$Category <- factor(data$Category, levels = data$Category[order(-data$Cost)])
-    min_cost = 800
-    data$PlotCost <- ifelse(data$Cost < min_cost, min_cost, data$Cost)
-    # Example data for plotting
-    cost_plot = ggplot(data, aes(x = Category, y = PlotCost, fill = Category)) +
-      geom_col(show.legend = FALSE, width = 0.8, position = position_dodge(width = 0.9)) +  # Adjust width for spacing
-      geom_text(aes(label = scales::dollar(Cost)), vjust = 1.2, family = "Helvetica", size = 8, color = "white", face = "bold", position = position_dodge(width = 0.5)) +  # Adjusted vjust and position dodge
-      scale_fill_manual(values = c("#D17329", "#048F63")) +
-      scale_x_discrete(expand = expansion(mult = c(0, 0))) +  # Remove expansion on x-axis
-      labs(
-        title = "Cost comparison", 
-        subtitle = paste("<span style='color:#048F63;'>", round(hours), " hours", "</span><span style='color:black;'> over</span><span style='color:#048F63;'>", round(months_difference), " months</span>")
-      ) +
-      theme_minimal() +
-      theme(
-        plot.margin = margin(30, 80, 30, 80),
-        text = element_text(family = "Helvetica"),  # Set general text properties
-        axis.text.x = element_text(face = "bold", size = 17),
-        axis.text.y = element_blank(),  # Hide y-axis text
-        panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(),
-        axis.title = element_blank(),
-        plot.title = element_text(family = "Helvetica", face = "bold", size = 40, hjust = 0.5, vjust = 1.5),  # Make title bold and larger
-        plot.subtitle = element_markdown(family = "Helvetica", face = "bold", size = 25, hjust = 0.5, vjust = 0.5), # Make subtitle bold and slightly smaller than title
-        axis.line.x = element_blank()  # Remove the default x-axis line
-      ) +
-      geom_segment(aes(x = 0.5, xend = length(unique(data$Category)) + 0.5, y = 0, yend = 0), size = 1, color = "black") +  # Add custom x-axis line
-      geom_rect(aes(xmin = 1.6, xmax = 2.4, ymin = PlotCost[1], ymax = PlotCost[2]), fill = "white", colour = "black", linetype = "dashed") +
-      geom_text(aes(x = 2, y = Cost[1]/2, label = paste("SAVE\n", scales::dollar(Cost[[1]] - Cost[[2]]))), color = "black", family = "Helvetica", size = 8, fontface = "bold")
-    cached_cost_comparison(cost_plot)
     output$costComparison <- renderPlot({
-      
-     cached_cost_comparison()
+      min_cost = 800
+      data$PlotCost <- ifelse(data$Cost < min_cost, min_cost, data$Cost)
+      # Example data for plotting
+        ggplot(data, aes(x = Category, y = PlotCost, fill = Category)) +
+        geom_col(show.legend = FALSE, width = 0.8, position = position_dodge(width = 0.9)) +  # Adjust width for spacing
+        geom_text(aes(label = scales::dollar(Cost)), vjust = 3, family = "Helvetica", size = 8, color = "white", face = "bold", position = position_dodge(width = 0.5)) +  # Adjusted vjust and position dodge
+        scale_fill_manual(values = c("#D17329", "#048F63")) +
+        scale_x_discrete(expand = expansion(mult = c(0, 0))) +  # Remove expansion on x-axis
+        labs(
+          title = "Cost comparison", 
+          subtitle = paste("<span style='color:#048F63;'>", round(hours), " hours", "</span><span style='color:black;'> over</span><span style='color:#048F63;'>", round(months_difference), " months</span>")
+        ) +
+        theme_minimal() +
+        theme(
+          
+          plot.margin = margin(30, 80, 30, 5),
+          text = element_text(family = "Helvetica"),  # Set general text properties
+          axis.text.x = element_text(face = "bold", size = 17),
+          axis.text.y = element_blank(),  # Hide y-axis text
+          panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank(),
+          axis.title = element_blank(),
+          plot.title = element_text(family = "Helvetica", face = "bold", size = 40, hjust = 0.5, vjust = 1.5),  # Make title bold and larger
+          plot.subtitle = element_markdown(family = "Helvetica", face = "bold", size = 25, hjust = 0.5, vjust = 0.5), # Make subtitle bold and slightly smaller than title
+          axis.line.x = element_blank(), # Remove the default x-axis line
+          #panel.border = element_rect(color = "black", fill = NA, size = 1),  # Add border to the panel area
+          #plot.background = element_rect(color = "grey", size = 1)  # Add border around the entire plot area
+        ) +
+        geom_segment(aes(x = 0.5, xend = length(unique(data$Category)) + 0.5, y = 0, yend = 0), size = 1, color = "black") +  # Add custom x-axis line
+        geom_rect(aes(xmin = 1.6, xmax = 2.4, ymin = PlotCost[1], ymax = PlotCost[2]), fill = "white", colour = "black", linetype = "dashed") +
+        geom_text(aes(x = 2,y = Cost[1]/2, label = paste("SAVE\n", scales::dollar(Cost[[1]] - Cost[[2]]))), color = "black", family = "Helvetica", size = 8, fontface = "bold")
+      #y = Cost[1]/2
     })
     output$savingsText <- renderUI({
-    HTML(paste0("Compared to a pay-per-hour tutor, you would <span style='color: #D17329; font-weight: bold; line-height: 3; '>save around $", round(tutor_cost - cost), "</span> by practising with Hey Lady!"))
+    HTML(paste0("Compared to a pay-per-hour tutor, you would <span style='color: #D17329; font-weight: bold; line-height: 2; '>save around $", round(tutor_cost - cost), "</span> by practising with Hey Lady!"))
     })
     output$monthsText <- renderUI({
       HTML(paste0("around <span style='color:#048F63;'>", round(hours), " hours", "</span><span style='color:black;'> over </span><span style='color:#048F63;'>", round(months_difference), " months</span>"))
     })
   })
+
  
 }
 
-runApp(shinyApp(ui, server))
-
+# Run the application 
+shinyApp(ui = ui, server = server)
